0d8398614493c1ca032836f7eb48e83a317ed621
Chen Lu <chenlucircle@gmail.com>
2021-08-11 12:10:10 -0400

    Update Requests.js

--- src/Requests.js
+++ src/Requests.js
@@ -64,7 +64,7 @@ function Requests(props){
       <div className="App">
        
           <div>
-            <h1>Welcome to the Bad debt workflow demo saljfsadf</h1>
+            <h1>Test github to azure devops git to Netlify</h1>
 {
   <br/>
  //            eslint-disable-next-line   
